73521f8caa4285fd7ba24f84621dabe2e702405b
Michal Bernát <113421230+michau6666@users.noreply.github.com>
2022-10-04 09:10:07 +0200

    Update Numbers.cs
    if the remainder after dividing the number by 2 is 0 it returns even, otherwise it returns odd

--- Open-Lab-03.04/Numbers.cs
+++ Open-Lab-03.04/Numbers.cs
@@ -1,12 +1,19 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._04
 {
     public class Numbers
     {
-        public string EvenOrOdd(int number)
+        public string EvenOrOdd(int number )
         {
-            throw new NotImplementedException();
+            if(number % 2 == 0)
+            {
+                return "even";
+            }
+            else 
+            {
+                return "odd";
+            }
         }
     }
 }
